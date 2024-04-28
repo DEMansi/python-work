ac2262b9aa050e70d272239fa65c579ed1728039
mansi patel <153804537+DEMansi@users.noreply.github.com>
2024-04-28 12:36:04 +0530

    Add files via upload

---
+++ Assignment/module 5/1.py
@@ -0,0 +1,8 @@
+'''
+
+Python is a powerful high-level programming language that can be possibly used in a lot of fields. 
+These fields can range from data science to automation and web development. It also has amazing 
+libraries and frameworks that include pandas, NumPy, PyTorch, selenium, OpenCV, bottle, pyramid, 
+flask, etc. One such framework in Python that is used for web development majorly is Django.
+
+'''
---
+++ Assignment/module 5/5.py
@@ -0,0 +1,14 @@
+
+'''
+
+
+In Django, a QuerySet is a collection of data from a database. A QuerySet is built up as a list of objects. QuerySets make it easier to get the data you actually need by allowing you to filter and order the data at an early stage. 
+
+
+from myapp.models import Post
+
+post = Post(title='My Title', content='My Content')
+post.save()
+
+
+'''
